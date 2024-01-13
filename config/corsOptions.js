90bec5a8ee19971ec346056482ff4e111352f9a4
SidOmIO <siddiqomar98@gmail.com>
2024-01-13 20:48:09 +0800

    Tutorial 11 JWT Authentication

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,9 @@
-const whitelist = [
-    'https://www.google.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
-];
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin:(origin, callback) => {
-        //remove '|| !origin' outside dev
-        if(whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
         }
